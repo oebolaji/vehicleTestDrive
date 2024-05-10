eed6d676fae529df19de7a768eca89d8131ddba4
oebolaji <obafemi.bolaji@stanbicibtc.com>
2024-05-10 03:00:28 +0100

    added ocelot gateway

---
+++ ApiGateway/Ocelot.cs
@@ -0,0 +1,78 @@
+﻿namespace ApiGateway
+{
+    public class Ocelot
+    {
+        {
+//  "Logging": {
+//    "LogLevel": {
+//      "Default": "Information",
+//      "Microsoft.AspNetCore": "Warning"
+//    }
+//  },
+//  "AllowedHosts": "*",
+//  "Routes": [
+//    {
+//      "DownStreamPathTemplate": "/api/vehicles",
+//      "DownStreamScheme": "https",
+//      "DownStreamHostAndPorts": [
+//        {
+//          "Host": "localhost",
+//          "Port": "7196"
+//        }
+//      ],
+//      "UpStreamPathTemplate": "/gateway/vehicles",
+//      "UpStreamHttpMethod": [ "GET", "POST" ]
+//},
+//    {
+//    "DownStreamPathTemplate": "/api/vehicles/{id}",
+//      "DownStreamScheme": "https",
+//      "DownStreamHostAndPorts": [
+//        {
+//        "Host": "localhost",
+//          "Port": "7196"
+//        }
+//      ],
+//      "UpStreamPathTemplate": "/gateway/vehicles/{id}",
+//      "UpStreamHttpMethod": [ "GET", "PUT", "DELETE" ]
+//    },
+//    {
+//    "DownStreamPathTemplate": "/api/customer",
+//      "DownStreamScheme": "https",
+//      "DownStreamHostAndPorts": [
+//        {
+//        "Host": "localhost",
+//          "Port": "7042"
+//        }
+//      ],
+//      "UpStreamPathTemplate": "/gateway/customer",
+//      "UpStreamHttpMethod": [ "POST" ]
+//    },
+//    {
+//    "DownStreamPathTemplate": "/api/Reservations",
+//      "DownStreamScheme": "https",
+//      "DownStreamHostAndPorts": [
+//        {
+//        "Host": "localhost",
+//          "Port": "7089"
+//        }
+//      ],
+//      "UpStreamPathTemplate": "/gateway/reservations",
+//      "UpStreamHttpMethod": [ "GET" ]
+//    },
+//    {
+//    "DownStreamPathTemplate": "/api/Reservations/{id}",
+//      "DownStreamScheme": "https",
+//      "DownStreamHostAndPorts": [
+//        {
+//        "Host": "localhost",
+//          "Port": "7089"
+//        }
+//      ],
+//      "UpStreamPathTemplate": "/gateway/reservations/{id}",
+//      "UpStreamHttpMethod": [ "PUT" ]
+//    }
+//  ]
+//}
+
+    }
+}
